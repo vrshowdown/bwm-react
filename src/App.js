cd6b6af0c79bd5254d1452ba954373fab0231018
vrshowdown <jmu3dx@gmail.com>
2022-03-11 12:22:44 -0500

    Adds part of rental detail and rental listing page with routing. Adds bootstrap and scss.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Route, Redirect } from 'react-router-dom';
+import  { RentalList } from './components/rental/RentalList';
+import  { RentalDetail } from './components/rental/RentalDetail';
+import { Header } from './shared/Header';
 import './App.css';
-
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+	  render() {
+		 return (
+			<BrowserRouter>
+			<div className='App'>
+				<Header />
+				<div  className='container'>
+					<Route exact path='/' render={()=>{return <Redirect to='/rentals' /> }}  />
+					<Route exact path="/rentals" component={RentalList} />
+					<Route exact path="/rentals/:id" component={RentalDetail} />
+				</div>
+			</div>
+			</BrowserRouter>
+		);
+	}
 }
-
 export default App;
