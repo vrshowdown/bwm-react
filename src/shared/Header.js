fd9beba593c4de5d8736b117f63f27bf6e5a9aff
vrshowdown <jmu3dx@gmail.com>
2022-03-15 15:39:18 -0400

    Adds redux architecture, routing. and restructuring of files

--- src/shared/Header.js
+++ src/shared/Header.js
@@ -1,9 +1,10 @@
+import { Link } from 'react-router-dom';
 import React from 'react';
 export function Header(){
 	return (
 		<nav className='navbar navbar-dark navbar-expand-lg'>
 			<div className='container'>
-				 <a className='navbar-brand' href=''>BookWithMe</a>
+				<Link className='navbar-brand' to='/rentals'>BookWithMe</Link>
 				<form className='form-inline my-2 my-lg-0'>
 					<input className='form-control mr-sm-2 bwm-search' type='search' placeholder='Try "New York"' aria-label='Search'></input>
 					 <button className='btn btn-outline-success my-2 my-sm-0 btn-bwm-search' type='submit'>Search</button>
@@ -15,8 +16,8 @@ export function Header(){
 					data-target='#navbarNavAltMarkup' 
 					aria-controls='navbarNavAltMarkup' 
 					aria-expanded='false' 
-					aria-label='Toggle navigation'>
-
+					aria-label='Toggle navigation'
+				>
 					<span className='navbar-toggler-icon'></span>
 				 </button>
 				<div className='collapse navbar-collapse' id='navbarNavAltMarkup'>
@@ -28,5 +29,4 @@ export function Header(){
 			</div>
 		</nav>
 	)
-
 }
