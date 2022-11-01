bcab019b2e88cfa869cde0065f2e0f208619f937
Jibreel Utley <jmu3dx@gmail.com>
2022-11-01 14:16:14 -0400

    Revert "Rental List Manipulate State, Render"

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React, { Component } from 'react';
-import  { RentalList } from './components/rental/RentalList';
 import { Header } from './shared/Header';
 import logo from './logo.svg';
 import './App.scss';
 class App extends Component {
 	  render() {
-
 		 return (
-		<div className="App">
-			<Header />
-			<div className='container'>
-				<RentalList/>
+			 <div className="App">
+				<Header />
+				I AM APP COMPONENT
 			</div>
-		</div>
 		);
 	}
 }
