0741f77f77c6d30bfd7ba927dce52cc895b4980e
Jibreel <jmu3dx@gmail.com>
2022-11-02 14:26:25 -0400

    Rental List Manipulate State, Render

--- src/components/rental/RentalList.js
+++ src/components/rental/RentalList.js
@@ -2,15 +2,23 @@ import React from 'react';
 import {RentalCard} from './RentalCard';
 export  class RentalList extends React.Component {
 	constructor(){
-		super()
+		super();
 		this.state = {
 			rentals: [1,2,3 ]
 		}
+		this.addRental = this.addRental.bind(this);
 	}
 	renderRentals(){
 		return this.state.rentals.map((rental)=>{
 			return (<RentalCard/>);
 		});
+	}
+	addRental(){
+		const rentals = this.state.rentals
+		rentals.push(1);
+		this.setState({
+			rentals
+		})
 	}
 	render(){
 		return(
@@ -19,6 +27,7 @@ export  class RentalList extends React.Component {
 				<div className='row'>
 					{this.renderRentals()}
 				</div>
+				<button onClick={this.addRental}>Click here</button>
 			</section>
 		);
 	}
