121ec2c0bafdfa849504c56ec04849623c8b3314
Jibreel <jmu3dx@gmail.com>
2022-11-02 18:04:08 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
+import  { RentalList } from './components/rental/RentalList';
 import { Header } from './shared/Header';
 import logo from './logo.svg';
 import './App.scss';
 class App extends Component {
 	  render() {
+
 		 return (
-			 <div className="App">
-				<Header />
-				I AM APP COMPONENT
+		<div className="App">
+			<Header />
+			<div className='container'>
+				<RentalList/>
 			</div>
+		</div>
 		);
 	}
 }
