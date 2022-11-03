4e9dec1b6ac708ea980407d31c01ee55ac5736ff
Jibreel <jmu3dx@gmail.com>
2022-11-03 16:05:56 -0400

    Add rental Detail, Add Routing, add data,

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Redirect } from 'react-router-dom';
 import  { RentalList } from './components/rental/RentalList';
+import  { RentalDetail } from './components/rental/RentalDetail';
 import { Header } from './shared/Header';
-import logo from './logo.svg';
 import './App.scss';
 class App extends Component {
 	  render() {
-
 		 return (
-		<div className="App">
-			<Header />
-			<div className='container'>
-				<RentalList/>
+			<BrowserRouter>
+			<div className='App'>
+				<Header />
+				<div  className='container'>
+					<Route exact path='/' render={()=>{return <Redirect to='/rentals' /> }}  />
+					<Route exact path="/rentals" component={RentalList} />
+					<Route exact path="/rentals/:id" component={RentalDetail} />
+				</div>
 			</div>
-		</div>
+			</BrowserRouter>
 		);
 	}
 }
