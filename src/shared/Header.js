2c99e66ef882c13d5f03c7a269602f4204307f9b
Jibreel <jmu3dx@gmail.com>
2022-11-10 15:41:36 -0500

    Installed Redux , fix source path, Restructure

--- src/shared/Header.js
+++ src/shared/Header.js
@@ -1,12 +1,12 @@
-
+import { Link } from 'react-router-dom';
 import React from 'react';
 export function Header(){
 	return (
 		<nav className='navbar navbar-dark navbar-expand-lg'>
 			<div className='container'>
-				 <a className='navbar-brand' href=''>BookWithMe</a>
+				<Link className='navbar-brand' to='/rentals'>BookWithMe</Link>
 				<form className='form-inline my-2 my-lg-0'>
-					<input className='form-control mr-sm-2 bwm-search' type='search' placeholder='Try New York' aria-label='Search'/>
+					<input className='form-control mr-sm-2 bwm-search' type='search' placeholder='Try "New York"' aria-label='Search'/>
 					 <button className='btn btn-outline-success my-2 my-sm-0 btn-bwm-search' type='submit'>Search</button>
 				 </form>
 				<button 
@@ -29,5 +29,4 @@ export function Header(){
 			</div>
 		</nav>
 	)
-
 }
